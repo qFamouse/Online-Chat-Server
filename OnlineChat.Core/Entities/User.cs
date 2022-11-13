cfcb15cb42d2934655c8be08b5348798b9ae9f56
Famouse <famouse_ru@mail.ru>
2022-11-13 16:27:50 +0300

    chore: adding user enitity with identity

---
+++ OnlineChat.Core/Entities/User.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Identity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OnlineChat.Core.Entities
+{
+    public class User : IdentityUser<int>
+    {
+        public string Name { get; set; }
+        public string Email { get; set; }
+    }
+}
