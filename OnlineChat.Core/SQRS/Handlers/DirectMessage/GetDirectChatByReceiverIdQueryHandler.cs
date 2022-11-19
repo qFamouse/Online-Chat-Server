b6fedf6b8ad57e9f26140ac8cebc1f626f56a77d
Famouse <famouse_ru@mail.ru>
2022-11-19 19:00:08 +0300

    chore: adding get direct messages by receiver id query

---
+++ OnlineChat.Core/SQRS/Handlers/DirectMessage/GetDirectChatByReceiverIdQueryHandler.cs
@@ -0,0 +1,34 @@
+﻿using MediatR;
+using OnlineChat.Core.Interfaces.Repositories;
+using OnlineChat.Core.SQRS.Queries.DirectMessage;
+using OnlineChat.Core.Views;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OnlineChat.Core.SQRS.Handlers.DirectMessage
+{
+    using OnlineChat.Core.Entities;
+    using OnlineChat.Core.Interfaces.Services;
+
+    public class GetDirectChatByReceiverIdQueryHandler :
+        IRequestHandler<GetDirectChatByReceiverIdQuery, IEnumerable<DirectMessage>>
+    {
+        private readonly IDirectMessageRepository _directMessageRepository;
+        private readonly IIdentityService _identityService;
+
+        public GetDirectChatByReceiverIdQueryHandler(IDirectMessageRepository directMessageRepository, IIdentityService identityService)
+        {
+            _directMessageRepository = directMessageRepository ?? throw new ArgumentNullException(nameof(directMessageRepository));
+            _identityService = identityService ?? throw new ArgumentNullException(nameof(identityService));
+        }
+
+        public async Task<IEnumerable<DirectMessage>> Handle(GetDirectChatByReceiverIdQuery request, CancellationToken cancellationToken)
+        {
+            IEnumerable<DirectMessage> messages = await _directMessageRepository.GetDirectMessagesByUsersId(_identityService.Id, request.ReceiverId);
+            return messages;
+        }
+    }
+}
---
+++ OnlineChat.Core/SQRS/Queries/DirectMessage/GetDirectChatByReceiverIdQuery.cs
@@ -0,0 +1,22 @@
+﻿using MediatR;
+using OnlineChat.Core.Views;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OnlineChat.Core.SQRS.Queries.DirectMessage
+{
+    using OnlineChat.Core.Entities;
+
+    public class GetDirectChatByReceiverIdQuery : IRequest<IEnumerable<DirectMessage>>
+    {
+        public int ReceiverId { get; set; }
+
+        public GetDirectChatByReceiverIdQuery(int receiverId)
+        {
+            ReceiverId = receiverId;
+        }
+    }
+}
