7fb86ea160ba505ca8d36b8cb95a25b7e6324e8c
Famouse <famouse_ru@mail.ru>
2022-11-19 18:58:51 +0300

    chore: adding send direct message by receiver id command

---
+++ OnlineChat.Core/SQRS/Commands/DirectMessage/SendDirectMessageByReceiverIdCommand.cs
@@ -0,0 +1,24 @@
+﻿using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OnlineChat.Core.SQRS.Commands.DirectMessage
+{
+    using OnlineChat.Core.Entities;
+    using OnlineChat.Core.Requests.DirectMessage;
+
+    public class SendDirectMessageByReceiverIdCommand : IRequest<DirectMessage>
+    {
+        public int ReceiverId { get; set; }
+        public string Message { get; set; }
+
+        public SendDirectMessageByReceiverIdCommand(SendDirectMessageByReceiverIdRequest request)
+        {
+            ReceiverId = request.ReceiverId;
+            Message = request.Message;
+        }
+    }
+}
---
+++ OnlineChat.Core/SQRS/Handlers/DirectMessage/SendDirectMessageByReceiverIdCommandHandler.cs
@@ -0,0 +1,44 @@
+﻿using MediatR;
+using OnlineChat.Core.SQRS.Commands.DirectMessage;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OnlineChat.Core.SQRS.Handlers.DirectMessage
+{
+    using Microsoft.AspNetCore.Identity;
+    using OnlineChat.Core.Entities;
+    using OnlineChat.Core.Interfaces.Repositories;
+    using OnlineChat.Core.Interfaces.Services;
+    using System.Threading;
+
+    internal class SendDirectMessageByReceiverIdCommandHandler : IRequestHandler<SendDirectMessageByReceiverIdCommand, DirectMessage>
+    {
+        // identity service
+        private readonly IDirectMessageRepository _directMessageRepository;
+        private readonly IIdentityService _identityService;
+
+        public SendDirectMessageByReceiverIdCommandHandler(IDirectMessageRepository directMessageRepository, IIdentityService identityService)
+        {
+            _directMessageRepository = directMessageRepository ?? throw new ArgumentNullException(nameof(directMessageRepository));
+            _identityService = identityService ?? throw new ArgumentNullException(nameof(identityService));
+        }
+
+        public async Task<DirectMessage> Handle(SendDirectMessageByReceiverIdCommand request, CancellationToken cancellationToken)
+        {
+            var directMessage = new DirectMessage()
+            {
+                SenderId = _identityService.Id,
+                ReceiverId = request.ReceiverId,
+                Message = request.Message,
+            };
+
+            directMessage = await _directMessageRepository.InsertAsync(directMessage);
+            await _directMessageRepository.Save();
+
+            return directMessage;
+        }
+    }
+}
