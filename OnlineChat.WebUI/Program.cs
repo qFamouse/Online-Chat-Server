fabcad70529741e241753fc36713e6b0f4ea1fc5
Famouse <famouse_ru@mail.ru>
2022-12-03 18:38:16 +0300

    refactor: changing the name of swagger

--- OnlineChat.WebUI/Program.cs
+++ OnlineChat.WebUI/Program.cs
@@ -73,7 +73,7 @@ builder.Services.AddIdentityCore<User>()
 
 builder.Services.AddSwaggerGen(c =>
 {
-    c.SwaggerDoc("v1", new OpenApiInfo { Title = "iTechArtPizzaDelivery.WebUI", Version = "v1" });
+    c.SwaggerDoc("v1", new OpenApiInfo { Title = "OnlineChat", Version = "v1" });
     c.AddSecurityDefinition("Bearer",
         new OpenApiSecurityScheme
         {
