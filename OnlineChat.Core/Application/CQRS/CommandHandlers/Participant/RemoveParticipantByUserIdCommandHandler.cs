f29f8cf262eeea9e738c37186e32d8475efb09d4
Famouse <famouse_ru@mail.ru>
2022-12-25 21:08:30 +0300

    chore: adding command for remove participant from conversation

---
+++ OnlineChat.Core/Application/CQRS/CommandHandlers/Participant/RemoveParticipantByUserIdCommandHandler.cs
@@ -0,0 +1,30 @@
+﻿using Application.CQRS.Commands.Participant;
+using Application.Interfaces.Repositories;
+using Application.Queries;
+using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Application.CQRS.CommandHandlers.Participant
+{
+    internal class RemoveParticipantByUserIdCommandHandler : IRequestHandler<RemoveParticipantByUserIdCommand, Unit>
+    {
+        private readonly IParticipantRepository _participantRepository;
+
+        public RemoveParticipantByUserIdCommandHandler(IParticipantRepository participantRepository)
+        {
+            _participantRepository = participantRepository ?? throw new ArgumentNullException(nameof(participantRepository));
+        }
+
+        public async Task<Unit> Handle(RemoveParticipantByUserIdCommand request, CancellationToken cancellationToken)
+        {
+            await _participantRepository.DeleteByIdAsync(request.ConversationId);
+            await _participantRepository.Save();
+
+            return Unit.Value;
+        }
+    }
+}
---
+++ OnlineChat.Core/Contracts/Requests/Participant/RemoveParticipantByUserIdRequest.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Contracts.Requests.Participant
+{
+    public class RemoveParticipantByUserIdRequest
+    {
+        public int ConversationId { get; set; }
+        public int UsertId { get; set; }
+    }
+}
