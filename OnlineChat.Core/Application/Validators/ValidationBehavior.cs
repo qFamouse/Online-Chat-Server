1423596005d361479ebf20a830671f6ef71cd850
Famouse <famouse_ru@mail.ru>
2022-12-25 17:01:43 +0300

    chore: changing Validation Behavior to normal view, set validate to validateAsync

--- OnlineChat.Core/Application/Validators/ValidationBehavior.cs
+++ OnlineChat.Core/Application/Validators/ValidationBehavior.cs
@@ -13,26 +13,23 @@ namespace Application.Validators
     {
         private readonly IEnumerable<IValidator<TRequest>> _validators;
 
-        public ValidationBehavior(IEnumerable<IValidator<TRequest>> validators) => _validators = validators;
+        public ValidationBehavior(IEnumerable<IValidator<TRequest>> validators)
+        {
+            _validators = validators;
+        }
 
         public async Task<TResponse> Handle(TRequest request, RequestHandlerDelegate<TResponse> next, CancellationToken cancellationToken)
         {
-            if (!_validators.Any())
+            if (_validators.Any())
             {
-                return await next();
-            }
-
-            var context = new ValidationContext<TRequest>(request);
-
-            var errorsDictionary = _validators
-                .Select(x => x.Validate(context))
-                .SelectMany(x => x.Errors)
-                .Where(x => x != null)
-                .ToList();
-
-            if (errorsDictionary.Any())
-            {
-                throw new ValidationException(errorsDictionary);
+                var context = new ValidationContext<TRequest>(request);
+                var validationResults = await Task.WhenAll(_validators.Select(v => v.ValidateAsync(context, cancellationToken)));
+                var failures = validationResults.SelectMany(r => r.Errors).Where(f => f != null).ToList();
+                 
+                if (failures.Count != 0)
+                {
+                    throw new ValidationException(failures);
+                }
             }
 
             return await next();
