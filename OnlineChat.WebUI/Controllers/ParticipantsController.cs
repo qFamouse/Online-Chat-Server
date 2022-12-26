af7954e05e5d4850796f2fa6bb5ff6325d67b892
Famouse <famouse_ru@mail.ru>
2022-12-26 20:08:00 +0300

    feat: adding new controller with api

---
+++ OnlineChat.WebUI/Controllers/ParticipantsController.cs
@@ -0,0 +1,39 @@
+﻿using Application.CQRS.Commands.Conversation;
+using Application.CQRS.Commands.DirectMessage;
+using Application.CQRS.Commands.Participant;
+using Contracts.Requests.Conversation;
+using Contracts.Requests.DirectMessage;
+using Contracts.Requests.Participant;
+using MediatR;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace OnlineChat.WebUI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ParticipantsController : ControllerBase
+    {
+        private readonly ISender _sender;
+
+        public ParticipantsController(ISender sender)
+        {
+            _sender = sender ?? throw new ArgumentNullException(nameof(sender));
+        }
+
+        [Authorize(Roles = "User")]
+        [HttpPost]
+        public async Task<ActionResult> AddPartisipantAsync([FromBody] AddParticipantByUserIdRequest request)
+        {
+            return Ok(await _sender.Send(new AddParticipantByUserIdCommand(request)));
+        }
+
+        [Authorize(Roles = "User")]
+        [HttpDelete]
+        public async Task<ActionResult> RemovePartisipantAsync(RemoveParticipantByUserIdRequest request)
+        {
+            return Ok(await _sender.Send(new RemoveParticipantByUserIdCommand(request)));
+        }
+    }
+}
