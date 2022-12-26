9dfbeda4d7e530ff07265ebe5f587ddfe5a8925d
Famouse <famouse_ru@mail.ru>
2022-12-26 14:49:46 +0300

    chore: adding conversation api

---
+++ OnlineChat.WebUI/Controllers/ConversationsController.cs
@@ -0,0 +1,44 @@
+﻿using Application.CQRS.Commands.Conversation;
+using Application.CQRS.Commands.DirectMessage;
+using Contracts.Requests.Conversation;
+using Contracts.Requests.DirectMessage;
+using MediatR;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace OnlineChat.WebUI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ConversationsController : ControllerBase
+    {
+        private readonly ISender _sender;
+
+        public ConversationsController(ISender sender)
+        {
+            _sender = sender ?? throw new ArgumentNullException(nameof(sender));
+        }
+
+        [Authorize(Roles = "User")]
+        [HttpPost]
+        public async Task<ActionResult> CreateConversationAsync([FromBody] CreateConversationRequest request)
+        {
+            return Ok(await _sender.Send(new CreateConversationCommand(request)));
+        }
+
+        [Authorize(Roles = "User")]
+        [HttpDelete("{id}")]
+        public async Task<ActionResult> DeleteConversationAsync(int id)
+        {
+            return Ok(await _sender.Send(new DeleteConversationByIdCommand(id)));
+        }
+
+        [Authorize(Roles = "User")]
+        [HttpPatch("{id}")]
+        public async Task<ActionResult> UpdateConversationAsync(int id, [FromBody] UpdateConversationByIdRequest request)
+        {
+            return Ok(await _sender.Send(new UpdateConversationByIdCommand(id, request)));
+        }
+    }
+}
