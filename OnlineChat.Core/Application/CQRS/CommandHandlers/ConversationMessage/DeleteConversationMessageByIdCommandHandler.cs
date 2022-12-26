1b0bb89bcb9a332c34d635783354323ef90888bb
Famouse <famouse_ru@mail.ru>
2022-12-26 14:45:08 +0300

    chore: adding command send delete conversation message by receider id

---
+++ OnlineChat.Core/Application/CQRS/CommandHandlers/ConversationMessage/DeleteConversationMessageByIdCommandHandler.cs
@@ -0,0 +1,27 @@
+﻿using Application.CQRS.Commands.Conversation;
+using Application.CQRS.Commands.ConversationMessage;
+using Application.Interfaces.Repositories;
+using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Application.CQRS.CommandHandlers.ConversationMessage
+{
+    internal class DeleteConversationMessageByIdCommandHandler : IRequestHandler<DeleteConversationMessageByIdCommand, Entities.ConversationMessage>
+    {
+        private readonly IConversationMessagesRepository _conversationMessagesRepository;
+
+        public DeleteConversationMessageByIdCommandHandler(IConversationMessagesRepository conversationMessagesRepository)
+        {
+            _conversationMessagesRepository = conversationMessagesRepository ?? throw new ArgumentNullException(nameof(conversationMessagesRepository));
+        }
+
+        public Task<Entities.ConversationMessage> Handle(DeleteConversationMessageByIdCommand request, CancellationToken cancellationToken)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ OnlineChat.Core/Application/CQRS/Commands/ConversationMessage/DeleteConversationMessageByIdCommand.cs
@@ -0,0 +1,19 @@
+﻿using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Application.CQRS.Commands.ConversationMessage
+{
+    public class DeleteConversationMessageByIdCommand : IRequest<Entities.ConversationMessage>
+    {
+        public int Id { get; set; }
+
+        public DeleteConversationMessageByIdCommand(int id)
+        {
+            Id = id;
+        }
+    }
+}
