f311948dc2bc9aa4dd0e99274d70f76c3e2dc9ad
Famouse <famouse_ru@mail.ru>
2022-12-26 19:43:58 +0300

    chore: complete validator

--- OnlineChat.Core/Application/Validators/Commands/Participant/AddParticipantByUserIdCommandValidator.cs
+++ OnlineChat.Core/Application/Validators/Commands/Participant/AddParticipantByUserIdCommandValidator.cs
@@ -1,6 +1,9 @@
 ﻿using Application.CQRS.Commands.Participant;
 using Application.Interfaces.Repositories;
+using Application.Queries;
 using FluentValidation;
+using Resources;
+using Services.Interfaces;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -11,13 +14,49 @@ namespace Application.Validators.Participant
 {
     internal sealed class AddParticipantByUserIdCommandValidator : AbstractValidator<AddParticipantByUserIdCommand>
     {
-        public AddParticipantByUserIdCommandValidator(IParticipantRepository participantRepository)
+        private readonly IParticipantRepository _participantRepository;
+        private readonly IConversationRepository _conversationRepository;
+        private readonly IIdentityService _identityService;
+
+        public AddParticipantByUserIdCommandValidator(IConversationRepository conversationRepository, IParticipantRepository participantRepository, IIdentityService identityService)
         {
-            // UserId not empty | ConversationID not empty
-            // User from identity service is participant
-            // UserId is exists
-            // ConversationID is exists
-            // UserId is not member ConversationId
+            _participantRepository = participantRepository ?? throw new ArgumentNullException(nameof(participantRepository));
+            _conversationRepository = conversationRepository ?? throw new ArgumentNullException(nameof(conversationRepository));
+            _identityService = identityService ?? throw new ArgumentNullException(nameof(identityService));
+
+            RuleFor(x => x.UsertId)
+                .NotEmpty();
+
+            RuleFor(x => x)
+                .MustAsync(MustNotBePartisipantOfConversation);
+
+            RuleFor(x => x.ConversationId)
+                .NotEmpty()
+                .MustAsync(_conversationRepository.ExistsAsync).WithMessage(Messages.NotFound)
+                .MustAsync(MustBePartisipantOfConversation).WithMessage(Messages.AccessDenied);
+        }
+
+        private async Task<bool> MustBePartisipantOfConversation(int conversationId, CancellationToken cancellationToken)
+        {
+            var currentUserId = _identityService.GetUserId();
+            var participant = await _participantRepository.GetByQueryAsync(new ParticipantQuery()
+            {
+                ConversationId = conversationId,
+                UserId = currentUserId
+            });
+
+            return participant != null;
+        }
+
+        private async Task<bool> MustNotBePartisipantOfConversation(AddParticipantByUserIdCommand command, CancellationToken cancellationToken)
+        {
+            var participant = await _participantRepository.GetByQueryAsync(new ParticipantQuery()
+            {
+                ConversationId = command.ConversationId,
+                UserId = command.UsertId
+            });
+
+            return participant == null;
         }
     }
 }
