a9f4173c58d21c8fda9848bf9ccd03e53a003d3a
Famouse <famouse_ru@mail.ru>
2023-02-01 17:17:52 +0300

    chore: standardization of the validation response by ProblemDetails

--- OnlineChat.Core/Application/Validators/ValidationBehavior.cs
+++ OnlineChat.Core/Application/Validators/ValidationBehavior.cs
@@ -3,8 +3,11 @@ using MediatR;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net;
 using System.Text;
 using System.Threading.Tasks;
+using Hellang.Middleware.ProblemDetails;
+using Microsoft.AspNetCore.Mvc;
 
 namespace Application.Validators
 {
@@ -20,16 +23,32 @@ namespace Application.Validators
 
         public async Task<TResponse> Handle(TRequest request, RequestHandlerDelegate<TResponse> next, CancellationToken cancellationToken)
         {
-            if (_validators.Any())
+            if (!_validators.Any())
             {
-                var context = new ValidationContext<TRequest>(request);
-                var validationResults = await Task.WhenAll(_validators.Select(v => v.ValidateAsync(context, cancellationToken)));
-                var failures = validationResults.SelectMany(r => r.Errors).Where(f => f != null).ToList();
-                 
-                if (failures.Count != 0)
-                {
-                    throw new ValidationException(failures);
-                }
+                return await next();
+            }
+
+            var context = new ValidationContext<TRequest>(request);
+
+            var errorsDictionary = (
+                    await Task.WhenAll(_validators
+                    .Select(x => x.ValidateAsync(context, cancellationToken)))
+                    )
+                .SelectMany(x => x.Errors)
+                .Where(x => x != null)
+                .GroupBy(
+                    x => x.PropertyName,
+                    x => x.ErrorMessage,
+                    (propertyName, errorMessages) => new
+                    {
+                        Key = propertyName,
+                        Values = errorMessages.Distinct().ToArray()
+                    })
+                .ToDictionary(x => x.Key, x => x.Values);
+
+            if (errorsDictionary.Any())
+            {
+                 throw new ProblemDetailsException(new ValidationProblemDetails(errorsDictionary));
             }
 
             return await next();
