691b270a3a1e21a445ea20b1d1d4e23fcc8d3187
qFamouse <famouse_ru@mail.ru>
2023-02-13 10:54:10 +0300

    chore: install hangfire

--- Online-Chat-Cron/OnlineChat.Cron/Program.cs
+++ Online-Chat-Cron/OnlineChat.Cron/Program.cs
@@ -1,3 +1,5 @@
+using Hangfire;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -6,6 +8,14 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+
+builder.Services.AddHangfire(x =>
+{
+    x.UseSqlServerStorage(builder.Configuration.GetConnectionString("HangfireConnection"));
+});
+
+builder.Services.AddHangfireServer();
 
 var app = builder.Build();
 
@@ -19,6 +29,8 @@ if (app.Environment.IsDevelopment())
 app.UseHttpsRedirection();
 
 app.UseAuthorization();
+
+app.UseHangfireDashboard();
 
 app.MapControllers();
 
