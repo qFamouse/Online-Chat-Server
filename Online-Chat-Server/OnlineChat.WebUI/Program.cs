caf34744c22ad16f69a7f78eb8ac8fd6bbd3f260
qFamouse <famouse_ru@mail.ru>
2023-03-13 10:08:33 +0300

    chore: adding serilog

--- Online-Chat-Server/OnlineChat.WebUI/Program.cs
+++ Online-Chat-Server/OnlineChat.WebUI/Program.cs
@@ -10,151 +10,175 @@ using Domain.Entities;
 using Hellang.Middleware.ProblemDetails;
 using EntityFramework.SqlServer;
 using OnlineChat.WebUI.Extensions;
+using Serilog;
 
-var builder = WebApplication.CreateBuilder(args);
+Log.Logger = new LoggerConfiguration()
+    .WriteTo.Console()
+    .CreateLogger();
 
-builder.Services.AddProblemDetails(setup =>
+try
 {
-    setup.IncludeExceptionDetails = (context, exception) =>
-        builder.Environment.IsDevelopment() || builder.Environment.IsStaging();
-});
+    Log.Information("Starting web host");
+
+    var builder = WebApplication.CreateBuilder(args);
+
+    builder.Services.AddProblemDetails(setup =>
+    {
+        setup.IncludeExceptionDetails = (context, exception) =>
+            builder.Environment.IsDevelopment() || builder.Environment.IsStaging();
+    });
 
 
-builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
-builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+    builder.Services.AddControllers();
 
-#region Custom Services
+    builder.Host.UseSerilog((ctx, lc) => lc
+        .WriteTo.Console());
 
-ConfigurationManager configuration = builder.Configuration;
-builder.Services.Configure<AuthenticationConfiguration>(configuration.GetSection("Authentication"));
-builder.Services.Configure<IdentityConfiguration>(configuration.GetSection("Identity"));
-builder.Services.Configure<AzureBlobConfiguration>(configuration.GetSection("AzureBlob"));
+    // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+    builder.Services.AddEndpointsApiExplorer();
+    builder.Services.AddSwaggerGen();
 
-builder.Services.AddSignalR();
+    #region Custom Services
 
-builder.Services.AddAutoMappers();
+    ConfigurationManager configuration = builder.Configuration;
+    builder.Services.Configure<AuthenticationConfiguration>(configuration.GetSection("Authentication"));
+    builder.Services.Configure<IdentityConfiguration>(configuration.GetSection("Identity"));
+    builder.Services.Configure<AzureBlobConfiguration>(configuration.GetSection("AzureBlob"));
 
-builder.Services.AddClerk(builder.Configuration.GetConnectionString("ClerkConnection"));
+    builder.Services.AddSignalR();
 
-builder.Services.AddMediatR();
+    builder.Services.AddAutoMappers();
 
-builder.Services.AddRepositories();
+    builder.Services.AddClerk(builder.Configuration.GetConnectionString("ClerkConnection"));
 
-builder.Services.AddAzureBlobStorage(builder.Configuration.GetConnectionString("AzureBlobConnection"));
+    builder.Services.AddMediatR();
 
-builder.Services.AddMassTransit();
+    builder.Services.AddRepositories();
 
-builder.Services.AddValidators();
+    builder.Services.AddAzureBlobStorage(builder.Configuration.GetConnectionString("AzureBlobConnection"));
 
-builder.Services.AddSingleton<IHttpContextAccessor, HttpContextAccessor>();
+    builder.Services.AddMassTransit();
 
-builder.Services.AddServices();
+    builder.Services.AddValidators();
 
-builder.Services.AddSqlServerDbContext(builder.Configuration.GetConnectionString("DefaultConnection"));
+    builder.Services.AddSingleton<IHttpContextAccessor, HttpContextAccessor>();
 
-builder.Services.AddCors(options =>
-{
-    options.AddPolicy("CorsPolicy", builder => builder
-        .WithOrigins("http://localhost:4200")
-        .AllowAnyMethod()
-        .AllowAnyHeader()
-        .AllowCredentials());
-});
+    builder.Services.AddServices();
 
-// JWT Authentication
+    builder.Services.AddSqlServerDbContext(builder.Configuration.GetConnectionString("DefaultConnection"));
 
-builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
-    .AddJwtBearer(
-        options =>
-        {
-            options.TokenValidationParameters = new TokenValidationParameters()
+    builder.Services.AddCors(options =>
+    {
+        options.AddPolicy("CorsPolicy", builder => builder
+            .WithOrigins("http://localhost:4200")
+            .AllowAnyMethod()
+            .AllowAnyHeader()
+            .AllowCredentials());
+    });
+
+    // JWT Authentication
+
+    builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
+        .AddJwtBearer(
+            options =>
             {
-                ValidateIssuer = false,
-                ValidateAudience = false,
-                ValidateLifetime = true,
-                ValidateIssuerSigningKey = true,
-                IssuerSigningKey = new SymmetricSecurityKey(
-                                Encoding.UTF8.GetBytes(configuration["Authentication:SecurityKey"]))
-            };
-            options.Events = new JwtBearerEvents
-            {
-                OnMessageReceived = context =>
+                options.TokenValidationParameters = new TokenValidationParameters()
                 {
-                    var accessToken = context.Request.Query["access_token"];
-                    var path = context.HttpContext.Request.Path;
-                    // if request to hub
-                    if (!string.IsNullOrEmpty(accessToken) &&
-                        (path.StartsWithSegments("/direct")))
-                    { // then get token from request
-                        context.Token = accessToken;
+                    ValidateIssuer = false,
+                    ValidateAudience = false,
+                    ValidateLifetime = true,
+                    ValidateIssuerSigningKey = true,
+                    IssuerSigningKey = new SymmetricSecurityKey(
+                        Encoding.UTF8.GetBytes(configuration["Authentication:SecurityKey"]))
+                };
+                options.Events = new JwtBearerEvents
+                {
+                    OnMessageReceived = context =>
+                    {
+                        var accessToken = context.Request.Query["access_token"];
+                        var path = context.HttpContext.Request.Path;
+                        // if request to hub
+                        if (!string.IsNullOrEmpty(accessToken) &&
+                            (path.StartsWithSegments("/direct")))
+                        {
+                            // then get token from request
+                            context.Token = accessToken;
+                        }
+
+                        return Task.CompletedTask;
                     }
+                };
+            }
+        );
 
-                    return Task.CompletedTask;
-                }
-            };
-        }
-     );
-
-builder.Services.AddIdentityCore<User>()
-    .AddRoles<IdentityRole<int>>()
-    .AddEntityFrameworkStores<OnlineChatContext>()
-    .AddDefaultTokenProviders();
+    builder.Services.AddIdentityCore<User>()
+        .AddRoles<IdentityRole<int>>()
+        .AddEntityFrameworkStores<OnlineChatContext>()
+        .AddDefaultTokenProviders();
 
 
-builder.Services.AddSwaggerGen(c =>
-{
-    c.SwaggerDoc("v1", new OpenApiInfo { Title = "OnlineChat", Version = "v1" });
-    c.AddSecurityDefinition("Bearer",
-        new OpenApiSecurityScheme
-        {
-            In = ParameterLocation.Header,
-            Description = @"JWT Authorization header using the Bearer scheme. 
+    builder.Services.AddSwaggerGen(c =>
+    {
+        c.SwaggerDoc("v1", new OpenApiInfo { Title = "OnlineChat", Version = "v1" });
+        c.AddSecurityDefinition("Bearer",
+            new OpenApiSecurityScheme
+            {
+                In = ParameterLocation.Header,
+                Description = @"JWT Authorization header using the Bearer scheme. 
                                       Enter 'Bearer' [space] and then your token in the text input below.
                                       Example: 'Bearer 12345abcdef'",
-            Name = "Authorization",
-            Type = SecuritySchemeType.ApiKey
-        });
-    c.AddSecurityRequirement(new OpenApiSecurityRequirement {
+                Name = "Authorization",
+                Type = SecuritySchemeType.ApiKey
+            });
+        c.AddSecurityRequirement(new OpenApiSecurityRequirement
+        {
+            {
+                new OpenApiSecurityScheme
+                {
+                    Reference = new OpenApiReference
                     {
-                        new OpenApiSecurityScheme
-                        {
-                            Reference = new OpenApiReference
-                            {
-                                Type = ReferenceType.SecurityScheme,
-                                Id = "Bearer"
-                            },
-                            Scheme = "oauth2",
-                            Name = "Bearer",
-                            In = ParameterLocation.Header,
-                        },
-                        new string[] { }
-                    }
-                });
-});
+                        Type = ReferenceType.SecurityScheme,
+                        Id = "Bearer"
+                    },
+                    Scheme = "oauth2",
+                    Name = "Bearer",
+                    In = ParameterLocation.Header,
+                },
+                new string[] { }
+            }
+        });
+    });
 
-#endregion
+    #endregion
 
-var app = builder.Build();
+    var app = builder.Build();
 
-// Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
-    app.UseSwagger();
-    app.UseSwaggerUI();
+    // Configure the HTTP request pipeline.
+    if (app.Environment.IsDevelopment())
+    {
+        app.UseSwagger();
+        app.UseSwaggerUI();
+    }
+
+    app.UseHttpsRedirection();
+
+    app.UseProblemDetails();
+
+    app.UseCors("CorsPolicy");
+
+    app.UseAuthentication();
+    app.UseAuthorization();
+
+    app.MapControllers();
+    app.MapHub<DirectMessageHub>("/direct");
+
+    app.Run();
 }
-
-app.UseHttpsRedirection();
-
-app.UseProblemDetails();
-
-app.UseCors("CorsPolicy");
-
-app.UseAuthentication();
-app.UseAuthorization();
-
-app.MapControllers();
-app.MapHub<DirectMessageHub>("/direct");
-
-app.Run();
+catch (Exception ex)
+{
+    Log.Fatal(ex, "Host terminated unexpectedly");
+}
+finally
+{
+    Log.CloseAndFlush();
+}
