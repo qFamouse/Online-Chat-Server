0944a449cde71387fabf43fa9a284fa500587cfb
qFamouse <famouse_ru@mail.ru>
2023-04-06 16:44:27 +0300

    chore: using tfa service in SignIn post processor

---
+++ Online-Chat-Server/OnlineChat.Core/Application/CQRS/Pipelines/Users/SignInUserQueryPostProcessor.cs
@@ -0,0 +1,28 @@
+﻿using Application.CQRS.Queries.Users;
+using Contracts.Views.User;
+using MediatR.Pipeline;
+using Services.Abstractions;
+
+namespace Application.CQRS.Pipelines.Users
+{
+    public class SignInUserQueryPostProcessor : IRequestPostProcessor<SignInUserQuery, UserAuthorizationView>
+    {
+        private readonly ITfaService _tfaService;
+
+        public SignInUserQueryPostProcessor
+        (
+            ITfaService tfaService
+        )
+        {
+            _tfaService = tfaService ?? throw new ArgumentNullException(nameof(tfaService));
+        }
+
+        public async Task Process(SignInUserQuery request, UserAuthorizationView response, CancellationToken cancellationToken)
+        {
+            if (response.IsTfaEnabled && response.IsAuthSuccessful)
+            {
+                await _tfaService.StartAuthenticationAsync(request.Email);
+            }
+        }
+    }
+}
